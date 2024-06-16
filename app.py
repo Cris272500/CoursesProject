49ccf287e9c733b9230956929ab69b5d05404596
Cris272500 <cristopherquincer@gmail.com>
2024-06-15 20:29:06 -0600

    se hace un register

--- app.py
+++ app.py
@@ -1,49 +1,18 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, redirect, flash, session
+from models import Usuario, db
+# con eso importamos la password hasheada
+from werkzeug.security import generate_password_hash, check_password_hash
+from config import Config
+from flask_session import Session
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-empleados = [ # "CONSULTA A BASE DE DATOS"
-    {
-        "id": 1,
-        "nombre": "Juan Pérez",
-        "edad": 28,
-        "departamento": "Ventas",
-        "salario": 35000,
-        "fecha_contratacion": "2019-04-21"
-    },
-    {
-        "id": 2,
-        "nombre": "María López",
-        "edad": 34,
-        "departamento": "Marketing",
-        "salario": 42000,
-        "fecha_contratacion": "2018-07-14"
-    },
-    {
-        "id": 3,
-        "nombre": "Carlos Sánchez",
-        "edad": 45,
-        "departamento": "Finanzas",
-        "salario": 58000,
-        "fecha_contratacion": "2012-01-12"
-    },
-    {
-        "id": 4,
-        "nombre": "Ana Torres",
-        "edad": 30,
-        "departamento": "Recursos Humanos",
-        "salario": 39000,
-        "fecha_contratacion": "2020-09-01"
-    },
-    {
-        "id": 5,
-        "nombre": "Luis Gómez",
-        "edad": 25,
-        "departamento": "IT",
-        "salario": 45000,
-        "fecha_contratacion": "2021-11-11"
-    }
-]
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'filesystem'
+Session(app)
+
+db.init_app(app)
 
 @app.route("/") # esto es un decorador
 def index():
@@ -52,7 +21,7 @@ def index():
 
     dinero = 200
 
-    return render_template("index.html", name=name, edad=edad, dinero=dinero, empleados=empleados)
+    return render_template("index.html", name=name, edad=edad, dinero=dinero)
 
 @app.route("/saludo/<nombre>")
 def saludo(nombre):
@@ -66,6 +35,44 @@ def login():
         return f"Hola {name}"
     else:
         return render_template("index.html")
+    
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        print(f"{username, password}")
+        # si los campos estan vacios
+        if not username or not password:
+            # es danger, porque es para errores
+            flash("Campos vacios", "danger")
+            return redirect("/register")
+        
+        # creamos un nuevo usuario
+        # crear una password segura
+        password_hash = generate_password_hash(password)
+
+        # si el usuario ya existe
+        usuario_existente = Usuario.query.filter_by(username=username).first()
+
+        if usuario_existente:
+            flash("El usuario ya existe", 'danger')
+            return redirect("/register")
+        
+        # si el usuario es nuevo
+        new_user = Usuario(username=username, password_hash=password_hash, email_user=email)
+
+        # guardamos a la base de datos con db
+        db.session.add(new_user)
+        db.session.commit()
+
+        flash("Usuario registrado!", 'success ')
+        return redirect("/")
+
+    else:
+        return render_template("register.html")
 
 @app.route("/about")
 def about():
