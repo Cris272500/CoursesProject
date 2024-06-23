ba80c939b291d4127d00b7996af241579b4c4a98
Cris272500 <cristopherquincer@gmail.com>
2024-06-23 08:25:03 -0600

    ✅se trabaja en front end

--- example/app.py
+++ example/app.py
@@ -14,6 +14,27 @@ def register():
     else:
         return render_template("register.html")
 
+@app.route("/crear", methods=["POST", "GET"])
+def crear():
+    if request.method == "POST":
+        pass
+    else:
+        return render_template("create.html")
+
+@app.route("/cursos", methods=['POST', 'GET'])
+def cursos():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template("cursos.html")
+
+@app.route("/curso_detalle", methods=['POST', 'GET'])
+def curso_detalle():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template("curso_detail.html")
+    
 # main
 if __name__ == '__main__':
     app.run(debug=True)
